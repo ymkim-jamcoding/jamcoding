6cc5f6d9d4c84fa1e7c9c2315a11ba56ed7f7dac
ymkim <ymkim.bundang@gmail.com>
2026-05-01 20:54:52 +0900

    Quartz sync: May 1, 2026, 8:54 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -77,7 +77,10 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({
+        markdownLinkResolution: "shortest",
+        openLinksInNewTab: true,
+      }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.Mindmap({
