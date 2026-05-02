75a19916de2adc1ab3debe10e4887105f82fdbdc
ymkim <ymkim.bundang@gmail.com>
2026-05-02 15:40:23 +0900

    Quartz sync: May 2, 2026, 3:40 PM

--- quartz/static/scripts/slide.js
+++ quartz/static/scripts/slide.js
@@ -27290,7 +27290,7 @@ Slide.prototype.expandVariables = function (contentOnly, content, expandResult) 
 var Navigation = require('./slideshow/navigation')
   , Events = require('./slideshow/events')
   , utils = require('../utils')
-  , Slide = require('./slide.js')
+  , Slide = require('./slide')
   , Parser = require('../parser')
   , macros = require('../macros')
   ;
