ff893566332998281447fea445ee9232368dab0c
ymkim <ymkim.bundang@gmail.com>
2026-05-03 22:33:45 +0900

    Quartz sync: May 3, 2026, 10:33 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -4,30 +4,30 @@ import { version } from "../../package.json"
 import { i18n } from "../i18n"
 
 interface Options {
-    links: Record<string, string>
+  links: Record<string, string>
 }
 
 export default ((opts?: Options) => {
-    const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-        const year = new Date().getFullYear()
-        const links = opts?.links ?? []
-        return (
-            <footer class={`${displayClass ?? ""}`}>
-                <ul>
-                    {Object.entries(links).map(([text, link]) => (
-                        <li>
-                            <a href={link} >{text}</a>
-                        </li>
-                    ))}
-                </ul>
-                <p>
-                    {/* {i18n(cfg.locale).components.footer.createdWith}{" "} */}
-                    Created by Yòmá with <a href="https://quartz.jzhao.xyz/" target="_blank">Quartz v{version}</a> © {year}
-                </p>
-            </footer>
-        )
-    }
+  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
+    const year = new Date().getFullYear()
+    const links = opts?.links ?? []
+    return (
+      <footer class={`${displayClass ?? ""}`}>
+        <ul>
+          {Object.entries(links).map(([text, link]) => (
+            <li>
+              <a href={link} >{text}</a>
+            </li>
+          ))}
+        </ul>
+        <p>
+          {/* {i18n(cfg.locale).components.footer.createdWith}{" "} */}
+          Created by 용만쌤 with <a href="https://quartz.jzhao.xyz/" target="_blank">Quartz v{version}</a> © {year}
+        </p>
+      </footer>
+    )
+  }
 
-    Footer.css = style
-    return Footer
+  Footer.css = style
+  return Footer
 }) satisfies QuartzComponentConstructor
